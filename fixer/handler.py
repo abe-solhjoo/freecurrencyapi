678a39631f0af4cf63fa1245869130e2d7a14c47
Ebrahim <eb.solhjoo@gmail.com>
2024-05-09 21:02:35 +0330

    Bump to version 0.2

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,15 @@
-def get_rates():
-    print("test")
+import requests
+import json
+
+URL = 'https://api.freecurrencyapi.com/v1/latest?apikey='
+
+
+def get_rates(api_key):
+    """
+    get currencies rates from api
+    :return: rates in json
+    """
+    response = requests.get(URL + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return response.status_code
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='fixer-demo',
-      version='0.1',
+      version='0.2',
       description='Fixer service demo package',
       url='https://github.com/abe-solhjoo/freecurrencyapi',
       author='Ebrahim',
       author_email='ab.solhjoo@gmail.com',
       license='MIT',
       packages=['fixer'],
+      install_requires=['requests'],
       zip_safe=False)
