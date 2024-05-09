dd4bb7c2562e9422e92c63a8f71547cc1ea2e529
Ebrahim <eb.solhjoo@gmail.com>
2024-05-09 20:10:37 +0330

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fixer-demo',
+      version='0.1',
+      description='Fixer service demo package',
+      url='https://github.com/abe-solhjoo/freecurrencyapi',
+      author='Ebrahim',
+      author_email='ab.solhjoo@gmail.com',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
